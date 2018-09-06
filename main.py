872f2dfcd64f7241ae6a5b0a93013a617b90021e
MayThuHtun <maythutun.ucsmdy@gmail.com>
2018-09-07 00:52:28 +0630

    Fixed main(codecs)

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import zg2uni
 
 input_file_name = sys.argv[1]
 output_file_name = sys.argv[2]
-input_file = codes.open(input_file_name, encoding='utf-8')
+input_file = codecs.open(input_file_name, encoding='utf-8')
 output_file = codecs.open(output_file_name, encoding='utf-8', mode='w')
 
 for input_line in input_file:
