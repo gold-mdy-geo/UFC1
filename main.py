ea8ad19fbb947e5ed4d77b954a8ab5a7c190157f
MayThuHtun <maythutun.ucsmdy@gmail.com>
2018-09-07 23:14:27 +0630

    Fixed main for upload to heroku

--- main.py
+++ main.py
@@ -1,23 +1,68 @@
-# -*- coding: utf-8 -*-
+from flask import Flask, render_template, url_for, request, jsonify
+import z2u
+import u2z
+import w2u
+import u2w
 
-# Usage: main.py inputfilename.ext outputfilename.ext
-# Example: main.py zawgyi.txt unicode.txt
+myapp=Flask(__name__)
 
-import codecs
-import uni2win
+@myapp.route("/")
+def main():
+    return render_template("index.html")
 
-import sys
+@myapp.route("/convert", methods=["POST"])
+def convert():
+    myinput = request.form['myinput']
+    myoutput = request.form['myoutput']
+    source = request.form['source']
 
-input_file_name = sys.argv[1]
-output_file_name = sys.argv[2]
-input_file = codecs.open(input_file_name, encoding='utf-8')
-output_file = codecs.open(output_file_name, encoding='utf-8', mode='w')
+    if myinput == "Zawgyi" and myoutput == "Unicode":
+        output = z2u.convert(source)
+        return jsonify({'output': output})
+    if myinput == "Zawgyi" and myoutput == "WinMyanmar":
+        output = z2u.convert(source)
+        output = u2w.convert(output)
+        return jsonify({'output': output})
+    if myinput == "Unicode" and myoutput == "Zawgyi":
+        output = u2z.convert(source)
+        return jsonify({'output': output})
+    if myinput == "Unicode" and myoutput == "WinMyanmar":
+        output = u2w.convert(source)
+        return jsonify({'output': output})
+    if myinput == "WinMyanmar" and myoutput == "Zawgyi":
+        output = w2u.convert(source)
+        output = u2z.convert(output)
+        return jsonify({'output': output})
+    if myinput == "WinMyanmar" and myoutput == "Unicode":
+        output = w2u.convert(source)
+        return jsonify({'output': output})
+    return jsonify({'output': source})
 
-for input_line in input_file:
-    input_line = uni2win.convert(input_line)
-    output_file.write(input_line)
-    output_file.flush()
+
+@myapp.route("/home")
+def home():
+    return render_template("home.html")
+
+@myapp.route("/partner")
+def partner():
+    return render_template("partner.html")
+
+@myapp.route("/resources")
+def resources():
+    return render_template("resources.html")
+
+@myapp.route("/tools")
+def tools():
+    return render_template("tools.html")
+
+@myapp.route("/work")
+def work():
+    return render_template("work.html")
+
+@myapp.route("/questions_answers")
+def questions_answers():
+    return render_template("questions_answers.html")
 
 
-input_file.close()
-output_file.close()
+if __name__ == "__main__":
+    myapp.run(debug=True)
