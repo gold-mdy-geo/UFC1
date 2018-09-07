13725643c9716dfba644e886f5794bd1f98d33d3
NyoNyoHtwe <lovelynyo31@gmail.com>
2018-09-06 23:46:33 -0800

    output unicode to winmyanmar

--- rununi2win.py
+++ rununi2win.py
@@ -7,7 +7,7 @@
 # Example: main.py zawgyi.txt unicode.txt
 
 import codecs
-import uni2win
+import rununi2win
 import sys
 
 input_file_name = sys.argv[1]
@@ -16,7 +16,7 @@ input_file = codecs.open(input_file_name,encoding='utf-8')
 output_file = codecs.open(output_file_name,encoding='utf-8', mode='w')
 
 for input_line in input_file:
-  input_line = uni2win.convert(input_line)
+  input_line = rununi2win.convert(input_line)
   output_file.write(input_line)
   output_file.flush()
 
